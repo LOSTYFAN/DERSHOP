0ff95091f7b554dff84e2a5ba91d20fea99d2dd1
first <1055172199@qq.com>
2019-12-04 13:11:37 +0800

    创建SpringBoot项目

---
+++ src/main/java/com/yfan/dershop/DershopApplication.java
@@ -0,0 +1,13 @@
+package com.yfan.dershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yfan/dershop/DershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfan.dershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
