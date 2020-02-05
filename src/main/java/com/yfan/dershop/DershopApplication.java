b7ccbe60c58e374e6370dcdbb53b2e98c19242da
first <1055172199@qq.com>
2020-02-05 21:15:23 +0800

    ///

--- src/main/java/com/yfan/dershop/DershopApplication.java
+++ src/main/java/com/yfan/dershop/DershopApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DershopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DershopApplication.class, args);
+        SpringApplication.run(DershopApplication.class, args);//
     }
 }
